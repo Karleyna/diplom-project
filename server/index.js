752d3516758c3fe9f14a00d8f22aed42d5d9c85d
Karleyna <bondarenko.k11@gmail.com>
2023-02-21 10:43:14 +0700

    add temp registration

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const PORT = process.env.PORT || 7000;
+const models = require('./models/models');
 const sequelize = require('./db');
-const Console = require("console");
+const cors = require('cors');
+const router = require('./routes/index')
+const PORT = process.env.PORT || 7000;
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
 
+app.use(errorHandler);
+
+// app.get('/', (req,res)=>{
+//     res.status(200).json({message: 'WORKing!'});
+// });
 const start = async () => {
     try {
         await sequelize.authenticate();
--- server/models/models.js
+++ server/models/models.js
@@ -2,5 +2,22 @@ const sequelize = require('../db');
 const {DataTypes} = require('sequelize');
 
 const User = sequelize.define('user',{
-        id: {}
+        id: { type: DataTypes.UUID, primaryKey: true},
+        email: { type: DataTypes.STRING, unique: true},
+        password: { type: DataTypes.STRING},
+        role: { type: DataTypes.STRING, defaultValue: "user"}
+        // references: {
+        //                 model: Role,// This is the column name of the referenced model
+        //                 key: 'name', // With PostgreSQL, it is optionally possible to declare when to check the foreign key constraint, passing the Deferrable type.
+        //                 deferrable: Deferrable.INITIALLY_IMMEDIATE
+        //         }
 });
+// const Role = sequelize.define('role',{
+//         name: { type: DataTypes.UUID, primaryKey: true}
+// });
+//
+// User.hasMany(Role)
+// Role.belongsTo(User)
+module.exports = {
+        User
+}
