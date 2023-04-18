916fadaf6eae9edc3e41a326c0fa5dad8ea70d4f
Karleyna <bondarenko.k11@gmail.com>
2023-04-18 16:20:26 +0700

    add middleware

--- server/routes/postsRouter.js
+++ server/routes/postsRouter.js
@@ -5,16 +5,16 @@ const postPropertyController = require('../controllers/postPropertyController');
 const checkRole = require('../middleware/checkRoleMiddleware');
 const authMiddleware = require('../middleware/authMiddleware.js')
 
-router.post('/', postsController.create);
+router.post('/', authMiddleware, checkRole('admin', 'trainer'), postsController.create);
 router.get('/', postsController.getAll);
 router.get('/:id', postsController.getOne);
-router.put('/:id', postsController.update);
-router.delete('/:id', postsController.delete);
+router.put('/:id', authMiddleware, checkRole('admin', 'trainer'), postsController.update);
+router.delete('/:id',authMiddleware, checkRole('admin', 'trainer'),  postsController.delete);
 
 //Properties
 router.get('/:postId/property/', postPropertyController.getAll)
 router.get('/:postId/property/:id', postPropertyController.getOne)
-router.post('/:postId/property/',  postPropertyController.create)
+router.post('/:postId/property/', authMiddleware, checkRole('admin', 'trainer'), postPropertyController.create)
 router.put('/:postId/property/:id', authMiddleware, checkRole('admin', 'trainer'), postPropertyController.update)
 router.delete('/:postId/property/:id', authMiddleware, checkRole('admin', 'trainer'), postPropertyController.delete)
 
